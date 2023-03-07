9062de8ae53fe37178879f0eb23182dbb6ca5b3e
dbwofla11 <mynok2714@gmail.com>
2023-03-07 12:27:29 +0900

    class 상속 + 은닉성 공부

---
+++ C_SHAP객체지향연습_class연습/obj_lan_private.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShap
+{
+    class Knight
+    {
+        private int id;
+        
+        public void Setup_id(int id)
+        {
+            this.id = id;
+        }
+
+    }
+
+
+    class obj_lan_private
+    {
+        static void Main(string[] args)
+        {
+            Knight a = new Knight();
+            a.Setup_id(1);
+            
+        }
+        
+    }
+}
--- C_SHAP객체지향연습_class연습/obj_language2.cs
+++ C_SHAP객체지향연습_class연습/obj_language2.cs
@@ -20,7 +20,12 @@ namespace CShap
         {
             Console.WriteLine("플래이어 생성자 호출");
         }
-
+       /* public player(int hp)   
+        {
+            this.hp = hp;
+            Console.WriteLine("오버로딩 생성자 호출 ");
+        }
+*/
     }
     // 클래스 - 자식클래스 
     public class rogue : player
@@ -33,16 +38,28 @@ namespace CShap
     }
     public class Kisa : player 
     {
-        Kisa()
+        Kisa() // 생성자 
         {
             Console.WriteLine("기사 생성자 호출");
         }
+      
+        
+
+        static public Kisa CreateKisa()
+        {
+            Kisa kisa = new Kisa();
+            kisa.hp = 1000;
+            kisa.attack = 100;
+            return kisa;
+        }
+
     }
 
     public class obj_language2
     {
         public static void Main()
         {
+            Kisa kisa = Kisa.CreateKisa();
 
         } 
 
