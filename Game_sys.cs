af215a7fe4e69d229de92025835eb84e11d7ba59
dbwofla11 <mynok2714@gmail.com>
2023-03-09 16:49:19 +0900

    stage construct

---
+++ Game_sys.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShap
+{
+    public enum GameStage
+    {
+        Lobby,
+        Town,
+        Field
+    }
+
+    class Game_sys
+    {
+
+    }
+}
--- Text_RPG_2.cs
+++ Text_RPG_2.cs
@@ -13,12 +13,16 @@ namespace CShap
     {
         public static void Main(string[] args)
         {
+           
+
             // 딱히 include  , import 같은게 필요없음 
             Player_class player = new Knight();
             Monster_class monster = new Zombie();
 
             int damage = player.GetAttack();
             monster.OnDamaged(damage);
+
+            Console.WriteLine(monster.GetHp());
         }
 
     }
