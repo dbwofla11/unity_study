69bf87d2b388257dd4040c190a2228601b8845e9
dbwofla11 <mynok2714@gmail.com>
2023-03-09 17:45:03 +0900

    stage compo complete

--- Game_sys.cs
+++ Game_sys.cs
@@ -18,6 +18,9 @@ namespace CShap
     {
         private GameStage stage = GameStage.Lobby;
         private Player_class player = null;
+        private Monster_class monster = null;
+        Random rand = new Random();
+
         public void Process()
         {
             switch (stage)
@@ -29,6 +32,7 @@ namespace CShap
                     ProcessTown();
                     break;
                 case GameStage.Field:
+                    ProcessField();
                     break;
             }
         }
@@ -81,5 +85,90 @@ namespace CShap
                 
             }
         }
+        private void CreateRandomMonster()
+        {
+            int randValue = rand.Next(0, 3);
+            switch (randValue)
+            {
+                case 0:
+                    monster = new Slime();
+                    Console.WriteLine("슬라임 등장 !!");
+                    break;
+                case 1:
+                    monster = new Orc();
+                    Console.WriteLine("오크 등장 !!");
+                    break;
+                case 2:
+                    monster = new Zombie();
+                    Console.WriteLine("좀비 등장 !!");
+                    break;
+
+            }
+        }
+
+        public void ProcessField()
+        {
+            Console.WriteLine("필드에 입장했습니다");
+            Console.WriteLine("[1] 싸우기");
+            Console.WriteLine("[2] 일정 확률로 마을 돌아가기");
+
+            CreateRandomMonster();
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    ProcessFight();
+                    break;
+                case "2":
+                    TryEscape();
+                    break;
+
+            }
+        }
+
+        private void ProcessFight()
+        {
+            while (true)
+            {
+                int damage = player.GetAttack();
+                monster.OnDamaged(damage);
+                if (monster.isDaed())
+                {
+                    Console.WriteLine("승리했습니다!!");
+                    Console.WriteLine($"플레이어의 남은 체력 {player.GetHp()}");
+                    break;
+                }
+
+                damage = monster.GetAttack();
+                player.OnDamaged(damage);
+                if (player.isDaed())
+                {
+                    Console.WriteLine("죽었습니다!!");
+                    Console.WriteLine($"몬스터의 남은 체력 {monster.GetHp()}");
+                    break;
+                }
+
+                Console.WriteLine($"현재 플레이어의 체력 : {player.GetHp()} , 현재 몬스터의 남은 체력 {monster.GetHp()}");
+
+
+            }
+        }
+
+        private void TryEscape()
+        {
+            int runvalue = rand.Next(0, 101);
+            if (runvalue < 33)
+            {
+                stage = GameStage.Town;
+            }
+            else
+            {
+                ProcessFight();
+            }
+        }
+
+   
+
     }
 }
